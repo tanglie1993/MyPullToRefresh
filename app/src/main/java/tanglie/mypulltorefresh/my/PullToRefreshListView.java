1fd4fa2d212b270d2643fb45542d6d97e9485778
tanglie1993 <tanglie23@163.com>
2016-08-21 18:43:33 +0800

    添加自动回滚

--- app/src/main/java/tanglie/mypulltorefresh/my/PullToRefreshListView.java
+++ app/src/main/java/tanglie/mypulltorefresh/my/PullToRefreshListView.java
@@ -1,27 +1,35 @@
 package tanglie.mypulltorefresh.my;
 
+import android.animation.Animator;
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.content.pm.LabeledIntent;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
 import android.widget.ArrayAdapter;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import tanglie.mypulltorefresh.R;
 
 /**
  * Created by Administrator on 2016/8/15 0015.
  */
 public class PullToRefreshListView extends LinearLayout {
 
+    private static final int RELEASE_TO_REFRESH_THRESHOLD = 300;
+
     private View headerView;
     private ListView listView;
 
-    private boolean isScrolling = false;
     private float currentDragStartY;
+    private State currentState = State.NO_OVERSCROLL;
 
     private int headerMaxHeight;
 
@@ -110,32 +118,82 @@ public class PullToRefreshListView extends LinearLayout {
         if(event.getAction() == MotionEvent.ACTION_DOWN){
             return listView.getFirstVisiblePosition() == 0 && listView.getChildAt(0).getTop() == 0;
         }else{
-            return isScrolling;
+            return true;
         }
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event){
         if(event.getAction() == MotionEvent.ACTION_DOWN){
-            isScrolling = true;
+            currentState = State.DRAGGING;
             currentDragStartY = event.getY();
             listView.onTouchEvent(event);
         }else if(event.getAction() == MotionEvent.ACTION_MOVE){
             if(event.getY() > currentDragStartY){
                 float deltaY = event.getY() - currentDragStartY;
+                if(deltaY > RELEASE_TO_REFRESH_THRESHOLD){
+                    currentState = State.RELEASE_TO_REFRESH;
+                    TextView headerTextView= (TextView) headerView.findViewById(R.id.headerTextView);
+                    headerTextView.setText("Release To Refresh");
+                }
                 onOverScroll(deltaY);
-            }else{
+            }else {
                 listView.onTouchEvent(event);
             }
         }else if(event.getAction() == MotionEvent.ACTION_UP){
             currentDragStartY = 0;
-            isScrolling = false;
             listView.onTouchEvent(event);
+            if(currentState == State.RELEASE_TO_REFRESH){
+                reset();
+            }else if(currentState == State.DRAGGING){
+                reset();
+            }
         }else if(event.getAction() == MotionEvent.ACTION_CANCEL){
             currentDragStartY = 0;
-            isScrolling = false;
             listView.onTouchEvent(event);
+            if(currentState == State.RELEASE_TO_REFRESH){
+                reset();
+            }else if(currentState == State.DRAGGING){
+                reset();
+            }
         }
         return true;
+    }
+
+    private void reset() {
+        currentState = State.RESETTING;
+        final ValueAnimator animator = ValueAnimator.ofFloat((float) headerView.getLayoutParams().height, (float) 0);
+        animator.setDuration(1000);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                float height = (Float) valueAnimator.getAnimatedValue();
+                onOverScroll(height);
+            }
+        });
+        animator.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animator) {
+                currentState = State.NO_OVERSCROLL;
+                TextView headerTextView = (TextView) headerView.findViewById(R.id.headerTextView);
+                headerTextView.setText("Pull To Refresh");
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animator) {
+
+            }
+        });
+        animator.start();
     }
 }
---
+++ app/src/main/java/tanglie/mypulltorefresh/my/State.java
@@ -0,0 +1,8 @@
+package tanglie.mypulltorefresh.my;
+
+/**
+ * Created by Administrator on 2016/8/21 0021.
+ */
+public enum State {
+    NO_OVERSCROLL, DRAGGING, RELEASE_TO_REFRESH, LOADING, RESETTING
+}
