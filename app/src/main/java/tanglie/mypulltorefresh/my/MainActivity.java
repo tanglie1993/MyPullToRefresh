a7b4f3136890b9ce9c66a599eecda82246709599
tanglie1993 <tanglie23@163.com>
2016-09-02 15:17:49 +0800

    实现每次加载结束时的刷新数据

--- app/src/main/java/tanglie/mypulltorefresh/my/MainActivity.java
+++ app/src/main/java/tanglie/mypulltorefresh/my/MainActivity.java
@@ -7,30 +7,39 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
 import android.widget.ArrayAdapter;
 
 import tanglie.mypulltorefresh.R;
 
 public class MainActivity extends Activity {
 
-
+    int itemCount = 20;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String[] strs = new String[20];
+        String[] strs = new String[itemCount];
         for(int i = 0; i < strs.length; i++){
             strs[i] = ""+i;
         }
+        final ArrayAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, strs);
         final PullToRefreshListView lv = (PullToRefreshListView) findViewById(R.id.listView);//得到ListView对象的引用 /*为ListView设置Adapter来绑定数据*/
-        lv.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, strs));
+        lv.setAdapter(adapter);
         lv.setLoadingStartListener(new PullToRefreshListView.LoadingStartListener() {
             @Override
             public void onLoadingStart() {
+
+                itemCount += 10;
+                final String[] stringArray = new String[itemCount];
+                for(int i = 0; i < itemCount; i++){
+                    stringArray[i] = ""+i;
+                }
                 new Handler().postDelayed(new Runnable() {
                     @Override
                     public void run() {
+                        lv.setAdapter(new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, stringArray));
                         lv.onLoadingFinish();
                     }
                 }, 2000);
